4656044b696bcff753ec17d205671c6384c225ad
Nhan Dang <nhantdang@gmail.com>
2020-07-14 16:17:19 +1000

    renamed build-bfs to build

--- .buildkite/test.sh
+++ .buildkite/test.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -euo pipefail
 echo "--- :docker: Docker compose build"
-docker-compose build build-bfs
+docker-compose build build
 echo "--- test"
-docker-compose run build-bfs
+docker-compose run build
