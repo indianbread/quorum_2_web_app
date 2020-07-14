e2d65d954b6a69351921adba793032075ba2ddcf
Nhan Dang <nhantdang@gmail.com>
2020-07-14 15:45:31 +1000

    testing sample code for docker image

--- .buildkite/test.sh
+++ .buildkite/test.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
-
 set -euo pipefail
 echo "--- :docker: Docker compose build"
-docker-compose build test
+docker-compose build build-bfs
 echo "--- test"
-docker-compose run test
+docker-compose run build-bfs
