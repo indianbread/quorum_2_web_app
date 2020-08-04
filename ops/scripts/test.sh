456542ad824f5b3d5e4fda9ca5bc024cbe47f8e1
Nhan Dang <nhantdang@gmail.com>
2020-08-04 11:11:26 +1000

    added ecr to pipeline

--- ops/scripts/test.sh
+++ ops/scripts/test.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -euo pipefail
 echo "--- :docker: Docker build"
-docker build -f Dockerfile --target test -t nhan-web-app:latest .
+docker build -f Dockerfile --target test -t nhan-web-app .
 echo "--- :docker: Docker run test"
-docker run --rm nhan-web-app:latest
+docker run --rm nhan-web-app
