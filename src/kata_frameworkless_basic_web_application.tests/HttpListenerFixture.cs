3c059500116664363e001b3097d82338e886229c
Nhan Dang <nhantdang@gmail.com>
2020-08-04 15:09:11 +1000

    user repository code wip

--- src/kata_frameworkless_basic_web_application.tests/HttpListenerFixture.cs
+++ src/kata_frameworkless_basic_web_application.tests/HttpListenerFixture.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Threading;
 using kata_frameworkless_web_app;
+using Microsoft.EntityFrameworkCore;
+using Moq;
 
 namespace kata_frameworkless_basic_web_application.tests
 {
@@ -8,11 +10,18 @@ namespace kata_frameworkless_basic_web_application.tests
     {
         private readonly BasicWebApp _basicWebApp;
         private Thread _webAppThread;
-        private readonly UserService _userService = new UserService(TODO);
+        private readonly UserService _userService;
+        private UserController _userController;
+        private UserRepository _userRepository;
+        private DbContext _dbContext;
 
         public HttpListenerFixture()
         {
-            _basicWebApp = new BasicWebApp(new UserController(_userService), _userService );
+            _dbContext = new SqLiteDbContext();
+            _userRepository = new UserRepository(_dbContext);
+            _userService = new UserService(_userRepository);
+            _userController = new UserController(_userService);
+            _basicWebApp = new BasicWebApp(_userController);
             _webAppThread = new Thread(_basicWebApp.Start);
             _webAppThread.Start();
         }
