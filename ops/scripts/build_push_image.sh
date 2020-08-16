3260c72f66fb53258b87b11f976f50a0ac70b0ea
Nhan Dang <nhantdang@gmail.com>
2020-08-17 09:53:01 +1000

    updated deploy scripts to use buildkite build number again

--- ops/scripts/build_push_image.sh
+++ ops/scripts/build_push_image.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 set -euo pipefail
+echo"--login to ECR--"
+aws ecr get-login-password --region ap-southeast-2 | docker login --username AWS --password-stdin 741922737521.dkr.ecr.ap-southeast-2.amazonaws.com
 echo "--- :docker: Docker build"
-docker build -f Dockerfile -t 741922737521.dkr.ecr.ap-southeast-2.amazonaws.com/nhan-frameworkless-app:latest2 .
+docker build -f Dockerfile -t 741922737521.dkr.ecr.ap-southeast-2.amazonaws.com/nhan-frameworkless-app:$1 .
 echo "--- :docker: Push image to ECR"
-docker push 741922737521.dkr.ecr.ap-southeast-2.amazonaws.com/nhan-frameworkless-app:latest2
+docker push 741922737521.dkr.ecr.ap-southeast-2.amazonaws.com/nhan-frameworkless-app:$1
