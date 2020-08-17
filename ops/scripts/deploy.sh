a151945a2a3b7207de9a53ccdbd7cc6dd8041e16
Nhan Dang <nhantdang@gmail.com>
2020-08-17 10:43:21 +1000

    modified docker login command

--- ops/scripts/deploy.sh
+++ ops/scripts/deploy.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -euo pipefail
 echo "login to ecr"
-docker login -u AWS -p $(aws ecr get-login-password --region ap-southeast-2) 741922737521.dkr.ecr.ap-southeast-2.amazonaws.com
+aws ecr get-login-password --region ap-southeast-2 | docker login --username AWS --password-stdin 741922737521.dkr.ecr.ap-southeast-2.amazonaws.com
 echo "--- :rocket: Jupiter Deploy"
 kubectl apply -n fma -f ./ops/deploy/jupiter_deploy.yml
 kubectl rollout status --watch=true deployment/nhan-frameworkless-web-app-deployment -n fma
