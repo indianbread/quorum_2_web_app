c80b4f277bb3dd0ac257006450bb204d1b8d85ee
Nhan Dang <nhantdang@gmail.com>
2020-08-17 12:21:32 +1000

    removed unused lines from build image script

--- ops/scripts/build_push_image.sh
+++ ops/scripts/build_push_image.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 set -euo pipefail
-#echo"--login to ECR--"
-#aws ecr get-login-password --region ap-southeast-2 | docker login --username AWS --password-stdin 741922737521.dkr.ecr.ap-southeast-2.amazonaws.com
 echo "--- :docker: Docker build"
 docker build -f Dockerfile -t 741922737521.dkr.ecr.ap-southeast-2.amazonaws.com/nhan-frameworkless-app:latest .
 echo "--- :docker: Push image to ECR"
