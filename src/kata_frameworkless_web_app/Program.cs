3fc1a3a1f9fe42a80c2d1b22b64ab771910100cd
Nhan Dang <nhantdang@gmail.com>
2020-08-18 12:01:11 +1000

    adding secret user lines back in to program

--- src/kata_frameworkless_web_app/Program.cs
+++ src/kata_frameworkless_web_app/Program.cs
@@ -15,8 +15,8 @@ namespace kata_frameworkless_web_app
             var userRepository = new UserRepository();
            // var userRepository = new ListRepository();
             var userService = new UserService(userRepository);
-           // var secretUser = AwsSecretManager.GetSecret();
-           // userService.AddSecretUserName(secretUser);
+            var secretUser = AwsSecretManager.GetSecret();
+            userService.AddSecretUserName(secretUser);
             var userController = new UserController(userService);
             var basicWebApp = new BasicWebApp(userController);
             basicWebApp.Start();
