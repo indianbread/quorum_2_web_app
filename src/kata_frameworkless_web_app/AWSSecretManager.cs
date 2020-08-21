43513c01360863b912ccef295ab33fc00501bb8d
Nhan Dang <nhantdang@gmail.com>
2020-08-21 17:47:45 +1000

    fixed get secret method

--- src/kata_frameworkless_web_app/AWSSecretManager.cs
+++ src/kata_frameworkless_web_app/AWSSecretManager.cs
@@ -20,35 +20,38 @@ namespace kata_frameworkless_web_app
 
             var memoryStream = new MemoryStream();
 
-            IAmazonSecretsManager client = new AmazonSecretsManagerClient(RegionEndpoint.APSoutheast2); //access key id & secret access key
+            IAmazonSecretsManager
+                client = new AmazonSecretsManagerClient(RegionEndpoint
+                    .APSoutheast2); //access key id & secret access key
 
             var request = new GetSecretValueRequest {SecretId = secretName, VersionStage = "AWSCURRENT"};
-            
+
             GetSecretValueResponse response = null;
 
             try
             {
-                response = client.GetSecretValueAsync(request).Result;
+                response = client.GetSecretValueAsync(request).GetAwaiter().GetResult();
+                if (response.SecretString != null)
+                {
+                    secret = response.SecretString;
+                }
+                else
+                {
+                    memoryStream = response.SecretBinary;
+                    var reader = new StreamReader(memoryStream);
+                    secret = System.Text.Encoding.UTF8.GetString(Convert.FromBase64String(reader.ReadToEnd()));
+                }
+
+                var secretObject = JObject.Parse(secret);
+                return ((secretObject["SecretUser"]) ?? string.Empty).Value<string>();
             }
+
             catch (Exception e)
             {
                 Console.WriteLine(e.Message);
+                throw e;
             }
-  
-            if (response.SecretString != null)
-            {
-                secret = response.SecretString;
-            }
-            else
-            {
-                memoryStream = response.SecretBinary;
-                var reader = new StreamReader(memoryStream);
-                secret = System.Text.Encoding.UTF8.GetString(Convert.FromBase64String(reader.ReadToEnd()));
-            }
-
-            var secretObject = JObject.Parse(secret);
-            return ((secretObject["SecretUser"]) ?? string.Empty).Value<string>() ;
+            
         }
-
     }
 }
