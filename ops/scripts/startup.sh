f1e0422f751ec76ca4929bfc579b83ef5b18e4f9
Nhan Dang <nhantdang@gmail.com>
2020-08-25 11:36:11 +1000

    trying to display s3 in stasndard output

--- ops/scripts/startup.sh
+++ ops/scripts/startup.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 eval $(aws s3 cp s3://nhan-secret-name/secret_name.txt - | sed 's/^/export /')
+aws s3 cp s3://nhan-secret-name/secret_name.txt -
 env
 dotnet kata_frameworkless_web_app.dll
