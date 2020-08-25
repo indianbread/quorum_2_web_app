0f55bec410c21ec261c71c3571a8a1e0bcf9f7d1
Nhan Dang <nhantdang@gmail.com>
2020-08-25 14:00:49 +1000

    testing fetching from s3

--- ops/scripts/startup.sh
+++ ops/scripts/startup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
+# Check that the environment variable has been set correctly
+
 #eval $(aws s3 cp s3://nhan-secret-name/secret_name.txt - | sed 's/^/export /')
-aws sts get-caller-identity -
 aws s3 cp s3://nhan-secret-name/secret_name.txt -
 #env
 dotnet kata_frameworkless_web_app.dll
