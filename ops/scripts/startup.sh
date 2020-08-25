8e06a0cb273d38060fe5d42fb700a4fcaab090cf
Nhan Dang <nhantdang@gmail.com>
2020-08-25 12:04:45 +1000

    just printing secret name to standard output

--- ops/scripts/startup.sh
+++ ops/scripts/startup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-eval $(aws s3 cp s3://nhan-secret-name/secret_name.txt - | sed 's/^/export /')
+#eval $(aws s3 cp s3://nhan-secret-name/secret_name.txt - | sed 's/^/export /')
 aws s3 cp s3://nhan-secret-name/secret_name.txt -
-env
+#env
 dotnet kata_frameworkless_web_app.dll
