ca37b898fb9963c219a2dd6ba3e3fcce9e312e04
Nhan Dang <nhantdang@gmail.com>
2020-08-25 13:41:20 +1000

    testing aws cli

--- ops/scripts/startup.sh
+++ ops/scripts/startup.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 #eval $(aws s3 cp s3://nhan-secret-name/secret_name.txt - | sed 's/^/export /')
+aws sts get-caller-identity -
 aws s3 cp s3://nhan-secret-name/secret_name.txt -
 #env
 dotnet kata_frameworkless_web_app.dll
