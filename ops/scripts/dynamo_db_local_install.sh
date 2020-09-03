cfb3203868c114ab49eb1ec2c14aab2bf1f92031
Nhan Dang <nhantdang@gmail.com>
2020-09-03 17:29:58 +1000

    fixing dynamo to not start up right away

--- ops/scripts/dynamo_db_local_install.sh
+++ ops/scripts/dynamo_db_local_install.sh
@@ -9,5 +9,5 @@ wget http://dynamodb-local.s3-website-us-west-2.amazonaws.com/dynamodb_local_lat
 gunzip dynamodb_local_latest.tar.gz
 mkdir ./dynamolocal
 tar -xf dynamodb_local_latest.tar -C ./dynamolocal
-cd ./dynamolocal
-java -Djava.library.path=./DynamoDBLocal_lib -jar DynamoDBLocal.jar -sharedDb
+#cd ./dynamolocal
+#java -Djava.library.path=./DynamoDBLocal_lib -jar DynamoDBLocal.jar -sharedDb
