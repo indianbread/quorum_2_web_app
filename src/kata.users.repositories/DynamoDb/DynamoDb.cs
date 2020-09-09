4c80f188e1a7519afab51135ef3f40ab27fe2828
Nhan Dang <nhantdang@gmail.com>
2020-09-09 16:18:20 +1000

    refactored dynamodb class to include access key id and access k to run dynamodb local

--- src/kata.users.repositories/DynamoDb/DynamoDb.cs
+++ src/kata.users.repositories/DynamoDb/DynamoDb.cs
@@ -7,18 +7,23 @@ namespace kata.users.repositories.DynamoDb
     {
         public static AmazonDynamoDBClient CreatClient(bool useDynamoDbLocal)
         {
-            var config = useDynamoDbLocal ? GetLocalConfig() : GetProductionConfig();
+            return useDynamoDbLocal ? GetLocalClient() : GetProductionClient();
+
+        }
+
+        private static AmazonDynamoDBClient GetLocalClient()
+        {
+            var config = new AmazonDynamoDBConfig {ServiceURL = "http://localhost:8000"};
+            const string AWS_ACCESS_KEY_ID="X";
+            const string AWS_SECRET_ACCESS_KEY = "X";
+            return new AmazonDynamoDBClient(AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY, config);
+            
+        }
+
+        private static AmazonDynamoDBClient GetProductionClient()
+        {
+            var config = new AmazonDynamoDBConfig() {RegionEndpoint = RegionEndpoint.APSoutheast2};
             return new AmazonDynamoDBClient(config);
-        }
-
-        private static AmazonDynamoDBConfig GetLocalConfig()
-        {
-            return new AmazonDynamoDBConfig {ServiceURL = "http://localhost:8000"};
-        }
-
-        private static AmazonDynamoDBConfig GetProductionConfig()
-        {
-            return new AmazonDynamoDBConfig() {RegionEndpoint = RegionEndpoint.APSoutheast2};
         }
         
     }
