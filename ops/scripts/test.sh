bd615fa6e007f03e2385caee0fc897759e44ade2
Nhan Dang <nhantdang@gmail.com>
2020-09-16 17:02:13 +1000

    fixed dockerfile name in test shell script

--- ops/scripts/test.sh
+++ ops/scripts/test.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 echo "--- :docker: Docker build"
-docker build -f Dockerfile.test -t nhan-frameworkless-app:$1 .
+docker build -f Dockerfile.Test -t nhan-frameworkless-app:$1 .
 echo "--- :docker: Docker run"
 docker run nhan-frameworkless-app:$1
