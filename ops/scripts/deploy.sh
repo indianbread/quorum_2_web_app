323f6b66fb69e8604787f6652ff0d7c8efd0fef3
Nhan Dang <nhantdang@gmail.com>
2020-09-17 14:15:24 +1000

    changed docker iamge tag to git commit SHA

--- ops/scripts/deploy.sh
+++ ops/scripts/deploy.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 set -euo pipefail
 echo "--- :rocket: Jupiter Deploy"
-#kubectl apply -n fma -f ./ops/deploy/jupiter_deploy.yml
 ktmpl ./ops/deploy/jupiter_deploy.yml -p imageTag $1  | kubectl apply -f -
 kubectl rollout status --watch=true deployment/nhan-frameworkless-web-app-deployment -n fma
