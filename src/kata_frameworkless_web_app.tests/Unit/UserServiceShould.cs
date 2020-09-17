6fde12c2edbaa3b60a04bdcdf99c432d7aa0f7ba
Nhan Dang <nhantdang@gmail.com>
2020-09-17 13:39:35 +1000

    readme created

--- src/kata_frameworkless_web_app.tests/Unit/UserServiceShould.cs
+++ src/kata_frameworkless_web_app.tests/Unit/UserServiceShould.cs
@@ -37,9 +37,9 @@ namespace kata_frameworkless_basic_web_application.tests.Unit
         {
             var user = await _sut.GetUserById("1");
 
-            //Assert.Equal("Nhan", user.FirstName);
+            Assert.Equal("Nhan", user.FirstName);
             //breaks pipeline
-            Assert.Equal("NotNhan", user.FirstName);
+            //Assert.Equal("NotNhan", user.FirstName);
 
         }
 
